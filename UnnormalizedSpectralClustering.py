[update] Shorten the calculation time of affinity matrix in Spectral Clustering [update] Change K for Spectral Clustering to len(users) * 0.05 [update] Fixed minor bugs in Spectral Clustering [add] Results that calculated by both algorithms [remove] Main.py

UnnormalizedSpectralClustering.py
@@ -2,9 +2,19 @@ import collections
 import math
 import numpy
 from scipy.sparse import csgraph
-from scipy.cluster.vq import vq, kmeans, whiten
+from scipy.cluster.vq import kmeans
 from scipy.spatial import distance
-from Tools import printProgressBar
+
+
+def printProgressBar(iteration, total, delimiter=None, prefix="", suffix="", decimals=1, length=100, fill="█", printEnd="\r"):
+    if iteration == total or delimiter is None or iteration % (delimiter * (total / 100)) == 0:
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + "-" * (length - filledLength)
+        print("\r%s |%s| %s%% %s" % (prefix, bar, percent, suffix), end=printEnd)
+        # Print New Line on Complete
+        if iteration == total:
+            print()
 
 
 def spectralClustering(A_orgInputMatrix, K):
@@ -28,14 +38,14 @@ def spectralClustering(A_orgInputMatrix, K):
             W_affinityMatrix[user_j, user_i] = W_affinityMatrix[user_i, user_j]
 
     print("Calculating L_laplacianMatrix...")
-    L_laplacianMatrix = csgraph.laplacian(W_affinityMatrix)
+    L_laplacianMatrix = csgraph.laplacian(W_affinityMatrix, normed=True)
 
     print("Calculating L_eigenvalues & L_eigenvectors...")
     L_eigenvalues, L_eigenvectors = numpy.linalg.eig(L_laplacianMatrix)
     L_eigenvaluesOrderedIndex = numpy.argsort(L_eigenvalues)
 
     print("\tEigenvalues: ", sorted(L_eigenvalues))
-    centroids, distortion = kmeans(L_eigenvectors[:, L_eigenvaluesOrderedIndex[:K]], K)
+    centroids, distortion = kmeans(numpy.real(L_eigenvectors[:, L_eigenvaluesOrderedIndex[:K]]), K)
 
     print("Assigning Clusters...")
     for userIndex in range(len(L_eigenvectors)):
@@ -49,13 +59,13 @@ def spectralClustering(A_orgInputMatrix, K):
         clustersUtoC[userIndex] = assignedCluster[0]
 
     print("\tAssigned Clusters: ", dict(clustersCtoU))
-    return clustersCtoU, clustersUtoC, int(round(mu_avgOfInputMatrix))
+    return clustersCtoU, clustersUtoC, int(mu_avgOfInputMatrix.round())
 
 
 def _unitTest():
     print("Loading Files...")
-    trainningData = numpy.genfromtxt("InputFiles/1.csv", delimiter=",", dtype=int)[1:, :-1]
-    testingData = numpy.genfromtxt("InputFiles/1_test.csv", delimiter=",", dtype="U10")
+    trainningData = numpy.genfromtxt("InputFiles/train.csv", delimiter=",", dtype=int)[1:, :-1]
+    testingData = numpy.genfromtxt("InputFiles/test.csv", delimiter=",", dtype="U10")
     A_orgInputHash = collections.defaultdict(lambda: {})
     clusterCtoI_Mu = collections.defaultdict(lambda: {})
     userDict, itemDict = {}, {}
@@ -79,7 +89,7 @@ def _unitTest():
     print("\n#Users:", len(userDict), "#Items: ", len(itemDict))
     del A_orgInputHash
 
-    clusterCtoU, clusterUtoC, mu_avgOfInputMatrix = spectralClustering(A_orgInputMatrix, 2)
+    clusterCtoU, clusterUtoC, mu_avgOfInputMatrix = spectralClustering(A_orgInputMatrix, int(len(userDict) * 0.05))
 
     print("Writing test result...")
     with open("OutputFiles/Yi-Chen Liu_preds_clustering.txt", "w", encoding="utf-8") as outputFile:
@@ -88,14 +98,18 @@ def _unitTest():
             if int(testingData[rowIndex, 1]) in userDict and int(testingData[rowIndex, 0]) in itemDict:
                 if not numpy.isnan(A_orgInputMatrix[userDict[int(testingData[rowIndex, 1])], itemDict[int(testingData[rowIndex, 0])]]):
                     predictRating = int(A_orgInputMatrix[userDict[int(testingData[rowIndex, 1])], itemDict[int(testingData[rowIndex, 0])]])
-                elif clusterUtoC[int(testingData[rowIndex, 1])] in clusterCtoI_Mu and int(testingData[rowIndex, 0]) in clusterCtoI_Mu[clusterUtoC[int(testingData[rowIndex, 1])]]:
+                elif clusterUtoC[userDict[int(testingData[rowIndex, 1])]] in clusterCtoI_Mu and itemDict[int(testingData[rowIndex, 0])] in clusterCtoI_Mu[clusterUtoC[userDict[int(testingData[rowIndex, 1])]]]:
                     predictRating = clusterCtoI_Mu[clusterUtoC[userDict[int(testingData[rowIndex, 1])]]][itemDict[int(testingData[rowIndex, 0])]]
                 else:
-                    predictRating = int(round(numpy.nanmean(A_orgInputMatrix[numpy.array(list(clusterCtoU[clusterUtoC[userDict[int(testingData[rowIndex, 1])]]])), itemDict[int(testingData[rowIndex, 0])]])))
+                    predictRating = numpy.nanmean(A_orgInputMatrix[numpy.array(list(clusterCtoU[clusterUtoC[userDict[int(testingData[rowIndex, 1])]]])), itemDict[int(testingData[rowIndex, 0])]])
+                    if numpy.isnan(predictRating):
+                        predictRating = mu_avgOfInputMatrix
+                    else:
+                        predictRating = int(predictRating.round())
                     clusterCtoI_Mu[clusterUtoC[userDict[int(testingData[rowIndex, 1])]]][itemDict[int(testingData[rowIndex, 0])]] = predictRating
                 outputFile.write(testingData[rowIndex, 0] + "," + testingData[rowIndex, 1] + "," + str(predictRating) + "," + testingData[rowIndex, 3] + "\n")
             else:
-                outputFile.write(testingData[rowIndex, 0] + "," + testingData[rowIndex, 1] + "," + str(mu_avgOfInputMatrix)+ "," + testingData[rowIndex, 3] + "\n")
+                outputFile.write(testingData[rowIndex, 0] + "," + testingData[rowIndex, 1] + "," + str(mu_avgOfInputMatrix) + "," + testingData[rowIndex, 3] + "\n")
         outputFile.close()
     return
 